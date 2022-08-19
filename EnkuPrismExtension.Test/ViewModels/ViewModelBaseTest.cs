6ef0ba5f332964b193a7de4a03cf265c7a6436f1
StdEnku <SyplessEleki@gmail.com>
2022-08-20 00:30:14 +0900

    コメント追記

--- EnkuPrismExtension.Test/ViewModels/ViewModelBaseTest.cs
+++ EnkuPrismExtension.Test/ViewModels/ViewModelBaseTest.cs
@@ -23,7 +23,10 @@ public class ViewModelBaseTest
     [Fact]
     public void KeepAlive_ゲッターの戻り値チェック_Trueを返す()
     {
+        // テスト対象の用意
         var viewModelBase = new ViewModelBase();
+
+        // 検証
         var result = viewModelBase.KeepAlive;
         Assert.True(result);
     }
@@ -33,7 +36,10 @@ public class ViewModelBaseTest
     [Fact]
     public void IsNavigationTarget_メソッドの戻り値チェック_Trueを返す()
     {
+        // テスト対象の用意
         var viewModelBase = new ViewModelBase();
+
+        // 検証
         var result = viewModelBase.IsNavigationTarget(this._navigationContext);
         Assert.True(result);
     }
@@ -43,7 +49,10 @@ public class ViewModelBaseTest
     [Fact]
     public void PersistInHistory_メソッドの戻り値チェック_Trueを返す()
     {
+        // テスト対象の用意
         var viewModelBase = new ViewModelBase();
+
+        //検証
         var result = viewModelBase.PersistInHistory();
         Assert.True(result);
     }
@@ -53,8 +62,11 @@ public class ViewModelBaseTest
     [Fact]
     public void NavigateCommand_OnNavigatedToメソッドが呼ばれる前にCanExecuteメソッド実行_Falseを返す()
     {
+        // テスト対象やダミーデータの用意
         const string DUMMY_VIEW_NAME = nameof(DUMMY_VIEW_NAME);
         var viewModelBase = new ViewModelBase();
+
+        // 検証
         var result = viewModelBase.NavigateCommand.CanExecute(DUMMY_VIEW_NAME);
         Assert.False(result);
     }
@@ -62,9 +74,12 @@ public class ViewModelBaseTest
     [Fact]
     public void NavigateCommand_OnNavigatedToメソッドが呼ばれた後にCanExecuteメソッド実行_Trueを返す()
     {
+        // テスト対象やダミーデータの用意
         const string DUMMY_VIEW_NAME = nameof(DUMMY_VIEW_NAME);
         var viewModelBase = new ViewModelBase();
         viewModelBase.OnNavigatedTo(this._navigationContext);
+
+        // 検証
         var result = viewModelBase.NavigateCommand.CanExecute(DUMMY_VIEW_NAME);
         Assert.True(result);
     }
@@ -72,29 +87,38 @@ public class ViewModelBaseTest
     [Fact]
     public void NavigateCommand_コマンド実行時_RegionNavigationServiceのRequestNavigateメソッドが実行される()
     {
+        // テスト対象やダミーデータの用意
         const string DUMMY_VIEW_NAME = nameof(DUMMY_VIEW_NAME);
         var viewModelBase = new ViewModelBase();
         viewModelBase.OnNavigatedTo(this._navigationContext);
         viewModelBase.NavigateCommand.Execute(DUMMY_VIEW_NAME);
+
+        // 検証
         this._regionNavigationService.Verify(rns => rns.RequestNavigate(new Uri(DUMMY_VIEW_NAME, UriKind.Relative), It.IsAny<Action<NavigationResult>>(), (NavigationParameters?)null));
     }
 
     [Fact]
     public void NavigateCommand_他Viewからの画面遷移直後_コマンドが実行可能かチェックされる()
     {
+        // テスト対象の用意
         var result = false;
         var viewModelBase = new ViewModelBase();
-        EventHandler? onCanExecuteChanged = null;
 
+        // CommandのCanExecuteChangedイベントにフックするイベントの用意
+        EventHandler? onCanExecuteChanged = null;
         onCanExecuteChanged = (sender, e) =>
         {
+            // CanExecuteChangedイベントが発生したらresultがtrueになる
             result = true;
             viewModelBase.NavigateCommand.CanExecuteChanged -= onCanExecuteChanged;
         };
-
         viewModelBase.NavigateCommand.CanExecuteChanged += onCanExecuteChanged;
         viewModelBase.OnNavigatedTo(this._navigationContext);
+
+        // NavigationContextのNavigatedイベントを発生させる
         this._regionNavigationService.Raise(rns => rns.Navigated += null, new RegionNavigationEventArgs(this._navigationContext));
+        
+        // 検証
         Assert.True(result);
     }
     #endregion
@@ -103,8 +127,11 @@ public class ViewModelBaseTest
     [Fact]
     public void GoBackCommand_OnNavigatedToメソッドが呼ばれる前にCanExecuteメソッド実行_Falseを返す()
     {
+        // テスト対象の用意
         var viewModelBase = new ViewModelBase();
         var result = viewModelBase.GoBackCommand.CanExecute();
+
+        // 検証
         Assert.False(result);
     }
 
@@ -113,9 +140,12 @@ public class ViewModelBaseTest
     [InlineData(false)]
     public void GoBackCommand_OnNavigatedToメソッドが呼ばれた後にCanExecuteメソッド実行_RegionNavigationJournalのCanGoBackプロパティと同じ値が返される(bool canGoBack)
     {
+        // テスト対象の用意
         this._regionNavigationJournal.Setup(rnj => rnj.CanGoBack).Returns(canGoBack);
         var viewModelBase = new ViewModelBase();
         viewModelBase.OnNavigatedTo(this._navigationContext);
+
+        // 検証
         var result = viewModelBase.GoBackCommand.CanExecute();
         Assert.Equal(result, canGoBack);
     }
@@ -123,29 +153,38 @@ public class ViewModelBaseTest
     [Fact]
     public void GoBackCommand_コマンド実行時_RegionNavigationJournalのGoBackメソッドが実行される()
     {
+        // テスト対象の用意
         this._regionNavigationJournal.Setup(rnj => rnj.CanGoBack).Returns(true);
         var viewModelBase = new ViewModelBase();
         viewModelBase.OnNavigatedTo(this._navigationContext);
         viewModelBase.GoBackCommand.Execute();
+
+        // 検証
         this._regionNavigationJournal.Verify(rnj => rnj.GoBack());
     }
 
     [Fact]
     public void GoBackCommand_他Viewからの画面遷移直後_コマンドが実行可能かチェックされる()
     {
+        // テスト対象の用意
         var result = false;
         var viewModelBase = new ViewModelBase();
-        EventHandler? onCanExecuteChanged = null;
 
+        // CommandのCanExecuteChangedイベントにフックするイベントの用意
+        EventHandler? onCanExecuteChanged = null;
         onCanExecuteChanged = (sender, e) =>
         {
+            // CanExecuteChangedイベントが発生したらresultがtrueになる
             result = true;
             viewModelBase.GoBackCommand.CanExecuteChanged -= onCanExecuteChanged;
         };
-
         viewModelBase.GoBackCommand.CanExecuteChanged += onCanExecuteChanged;
         viewModelBase.OnNavigatedTo(this._navigationContext);
+
+        // NavigationContextのNavigatedイベントを発生させる
         this._regionNavigationService.Raise(rns => rns.Navigated += null, new RegionNavigationEventArgs(this._navigationContext));
+        
+        // 検証
         Assert.True(result);
     }
     #endregion
@@ -154,8 +193,11 @@ public class ViewModelBaseTest
     [Fact]
     public void GoForwardCommand_OnNavigatedToメソッドが呼ばれる前にCanExecuteメソッド実行_Falseを返す()
     {
+        // テスト対象の用意
         var viewModelBase = new ViewModelBase();
         var result = viewModelBase.GoForwardCommand.CanExecute();
+
+        // 検証
         Assert.False(result);
     }
 
@@ -164,9 +206,12 @@ public class ViewModelBaseTest
     [InlineData(false)]
     public void GoForwardCommand_OnNavigatedToメソッドが呼ばれた後にCanExecuteメソッド実行_RegionNavigationJournalのCanGoBackプロパティと同じ値が返される(bool canGoForward)
     {
+        // テスト対象の用意
         this._regionNavigationJournal.Setup(rnj => rnj.CanGoForward).Returns(canGoForward);
         var viewModelBase = new ViewModelBase();
         viewModelBase.OnNavigatedTo(this._navigationContext);
+
+        // 検証
         var result = viewModelBase.GoForwardCommand.CanExecute();
         Assert.Equal(result, canGoForward);
     }
@@ -174,29 +219,38 @@ public class ViewModelBaseTest
     [Fact]
     public void GoForwardCommand_コマンド実行時_RegionNavigationJournalのGoForwardメソッドが実行される()
     {
+        // テスト対象の用意
         this._regionNavigationJournal.Setup(rnj => rnj.CanGoForward).Returns(true);
         var viewModelBase = new ViewModelBase();
         viewModelBase.OnNavigatedTo(this._navigationContext);
         viewModelBase.GoForwardCommand.Execute();
+
+        // 検証
         this._regionNavigationJournal.Verify(rnj => rnj.GoForward());
     }
 
     [Fact]
     public void GoForwardCommand_他Viewからの画面遷移直後_コマンドが実行可能かチェックされる()
     {
+        // テスト対象の用意
         var result = false;
         var viewModelBase = new ViewModelBase();
-        EventHandler? onCanExecuteChanged = null;
 
+        // CommandのCanExecuteChangedイベントにフックするイベントの用意
+        EventHandler? onCanExecuteChanged = null;
         onCanExecuteChanged = (sender, e) =>
         {
+            // CanExecuteChangedイベントが発生したらresultがtrueになる
             result = true;
             viewModelBase.GoForwardCommand.CanExecuteChanged -= onCanExecuteChanged;
         };
-
         viewModelBase.GoForwardCommand.CanExecuteChanged += onCanExecuteChanged;
         viewModelBase.OnNavigatedTo(this._navigationContext);
+
+        // NavigationContextのNavigatedイベントを発生させる
         this._regionNavigationService.Raise(rns => rns.Navigated += null, new RegionNavigationEventArgs(this._navigationContext));
+        
+        // 検証
         Assert.True(result);
     }
     #endregion
