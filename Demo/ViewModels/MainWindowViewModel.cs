9ac5603b2314e7da916ba8df4f9469e817ec9515
StdEnku <SyplessEleki@gmail.com>
2022-08-20 02:54:27 +0900

    demo修正

--- Demo/ViewModels/MainWindowViewModel.cs
+++ Demo/ViewModels/MainWindowViewModel.cs
@@ -12,6 +12,8 @@ public class MainWindowViewModel : BindableBase
     private void onNavigated(NavigationResult navigationResult)
     {
         System.Diagnostics.Debug.WriteLine("navigated");
+        this.RootRegionGoBackCommand.CheckCanExecute();
+        this.RootRegionGoForwardCommand.CheckCanExecute();
     }
 
     private NavigationParameters onNavigationg()
@@ -20,8 +22,37 @@ public class MainWindowViewModel : BindableBase
         return new NavigationParameters();
     }
 
+    public RegionGoBackCommand RootRegionGoBackCommand { get; private set; }
+    private void onGoBacking()
+    {
+        System.Diagnostics.Debug.WriteLine("backing");
+    }
+
+    private void onGoBacked()
+    {
+        System.Diagnostics.Debug.WriteLine("backed");
+        this.RootRegionGoBackCommand.CheckCanExecute();
+        this.RootRegionGoForwardCommand.CheckCanExecute();
+    }
+
+    public RegionGoForwardCommand RootRegionGoForwardCommand { get; private set; }
+    private void onGoForwarding()
+    {
+        System.Diagnostics.Debug.WriteLine("forwarding");
+    }
+
+    private void onGoForwarded()
+    {
+        System.Diagnostics.Debug.WriteLine("forwarded");
+        this.RootRegionGoBackCommand.CheckCanExecute();
+        this.RootRegionGoForwardCommand.CheckCanExecute();
+    }
+
+
     public MainWindowViewModel(IRegionManager regionManager)
     {
         this.RootRegionNavigateCommand = new(regionManager, RegionNames.RootRegion, this.onNavigationg, this.onNavigated);
+        this.RootRegionGoBackCommand = new(regionManager, RegionNames.RootRegion, this.onGoBacking, this.onGoBacked);
+        this.RootRegionGoForwardCommand = new(regionManager, RegionNames.RootRegion, this.onGoForwarding, this.onGoForwarded);
     }
 }
